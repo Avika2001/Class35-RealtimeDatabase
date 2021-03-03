1ecddc5b469d723cfe3c1b2a6918c73d06798d07
Avika Kapoor <Avika@Vertikas-MacBook-Pro.local>
2021-03-03 17:00:32 +0000

    realtime database

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 var ball;
+var database;
+var position;
 
 function setup(){
     createCanvas(500,500);
+    database=firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var location=database.ref("Ball/Position");
+    location.on("value",readPos,showError);
 }
 
 function draw(){
@@ -24,6 +29,18 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref("Ball/Position").set({
+        x:position.x+x,
+        y:position.y+y
+    })
+}
+
+function readPos(data){
+    position=data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("Error");
 }
